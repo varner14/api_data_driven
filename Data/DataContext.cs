7d9a8124bc7932da51a4a3167ba93310415978f9
VarnerCardoso <varner.silveira@gmail.com>
2021-11-15 19:35:25 -0300

    create new services token and migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,7 +6,6 @@ namespace Shop.Data
   public class DataContext : DbContext
   {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<User> Users { get; set; }
